4d81d1c9efc190e44942332fe175f49b2ef2112d
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-01-04 21:34:23 -0500

    Set up and connect to database

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const morgan = require("morgan");
-const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Import routes
-const postRoutes = require("./routes/post")
+const postRoutes = require("./routes/post");
 
+// Initialize app
+const app = express();
 
+// Connect to Database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Successfully connected to the Database"))
+  .catch((err) => console.log("Database connection error", err));
+
+// Global middlewares (to be used on all routes)
 app.use(morgan("dev"));
 
-app.use("/api", postRoutes )
+app.use("/api", postRoutes);
 
 const port = process.env.PORT || 8080;
 
